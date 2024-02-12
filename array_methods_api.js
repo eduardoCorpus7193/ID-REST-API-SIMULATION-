0ae35ce2b8ed924e9a58c37a96eaea529147313b
Eduardo Corpus <utm22030699@utma.edu.mx>
2024-02-12 08:33:00 -0600

    feat: array_methods is now working

---
+++ array_methods_api.js
@@ -0,0 +1,132 @@
+const books = require('./books.json');
+
+function getBook(searchTerm) {  
+    const foundBook = books.find(book => book.title === searchTerm || book.ISBN === searchTerm);
+    if(foundBook){
+        return {
+            code: 200,
+            body: foundBook,
+            msg: "Book '" + foundBook.title + "' found",
+        }
+    }
+    return {
+        code: 400,
+        body: null,
+        msg: "Couldn't find the book",
+    }
+}
+function getBooks() {
+    return {
+        code: 200,
+        body: books,
+        msg: "All books",
+    }
+}
+
+function addBook(newBook) {
+    books.push(newBook);
+    return {
+        code: 201,
+        body: books,
+        msg: "Book added",
+    }
+}
+
+function removeBookByTitleOrISBN(searchTerm) {
+    const index = books.findIndex(book => book.title === searchTerm || book.ISBN === searchTerm);
+    if(index !== -1){
+        const deletedBookName = books[index].title;
+        const deletedBook = books.splice(index,1)[0];
+        return {
+            code: 200,
+            body: books,
+            msg: "Book " + deletedBookName + " deleted",
+        }
+    }
+    return {
+        code: 400,
+        body: null,
+        msg: "Couldn't find the book",
+    }
+}
+
+function filterBy(property,searchTerm) {
+    const filteredBooks = books.filter(book => book[property] === searchTerm);
+    return {
+        code: 200,
+        body: filteredBooks,
+        msg: "Filtered books",
+    }
+}
+
+function listBooks() {  
+    const formattedBooks = books.map(book => ${book.title} - ${book.author} - ${book.year});
+    return {
+        code: 200,
+        body: formattedBooks,
+        msg: "Listed books",
+    }
+}
+
+function getBooksByYear(year) {
+    const filteredBooks = books.filter(book => book.year === year);
+    return {
+        code: 200,
+        body: filteredBooks,
+        msg: "Filtered books by the year: " + year,
+    }
+}
+
+function genreFullAvailability(genre) {  
+    const allAvailable = books.every(book => book.genre === genre && book.stock > 0);
+    if (allAvailable) {
+        return {
+            code: 200,
+            body: "True",
+            msg: "Genre " + genre + " is full available",
+        }
+    }
+    return {
+        code:   400 ,
+        body: "False",
+        msg: "Genre " + genre + " is not full available",
+    }
+}
+
+
+function genrePartialAvailability(genre) {
+    const partialAvailable = books.filter(book => book.genre === genre && book.stock > 0);
+    if(partialAvailable.length === 0){
+        return {
+            code: 400,
+            body: "False",
+            msg: "Genre" + genre + " is not available",
+        }
+    }
+    return {
+        code: 200,
+        body: "True",
+        msg: "Genre " + genre + " is available at least once",
+    }
+}
+
+function getCountBy(property, searchTerm) {
+    const count = books.filter(book => book[property] === searchTerm).length;
+    return {
+        code: 200,
+        body: count,
+        msg: "Count of books by " + property + " " + searchTerm,
+    } 
+}
+
+console.log(getBook("9781408855652"));
+console.log(getBooks());
+console.log(addBook({ "title": "The hunger games", "ISBN": "9780439023528", "year": 2008, "genre": "Adventure, science fiction", "author": "Suzanne Collins", "stock": 7, "publisher": "Scholastic Press" }));
+console.log(removeBookByTitleOrISBN("9780984782857"));
+console.log(filterBy("author","Suzanne Collins"));
+console.log(listBooks());
+console.log(getBooksByYear(2008));
+console.log(genreFullAvailability("Fantasy"));
+console.log(genrePartialAvailability("Fantasy"));
+console.log(filterBy("author","Suzanne Collins"));
+console.log(getCountBy('year', 2018));
